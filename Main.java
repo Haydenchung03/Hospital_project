8c27f4b75432fdb7246aeafd87c22eb2bb278d89
Haydenchung03 <Haydenchung21@hotmail.com>
2022-04-15 20:37:22 -0400

    Main menu

--- Main.java
+++ Main.java
@@ -4,15 +4,37 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Please enter in a username: ");
+        while (true) {
+            System.out.println("Main Menu");
+            System.out.println("Login");
+            System.out.println("Create Account");
+            System.out.println("Quit");
+            String option = scan.nextLine();
+            if (option.equalsIgnoreCase("LOGIN")) {
 
-        String username = scan.nextLine();
+                System.out.println("Please enter in a username: ");
 
-        System.out.println("Please enter in a password: ");
-        String password = scan.nextLine();
+                String username = scan.nextLine();
 
-        Login login = new Login(username, password);
-        System.out.println(login.print());
+                System.out.println("Please enter in a password: ");
+                String password = scan.nextLine();
+
+                Login login = new Login(username, password);
+                System.out.println(login.print());
+            } else if (option.equalsIgnoreCase("CREATE ACCOUNT")) {
+                System.out.println("Enter in a username");
+                String newUsername = scan.nextLine();
+                System.out.println("Enter in a password");
+                String newPassword = scan.nextLine();
+                System.out.println("Re-enter password");
+                String rePassword = scan.nextLine();
+
+            } else if (option.equalsIgnoreCase("Quit")) {
+                System.out.println("Thank you for using our program! Thank you!");
+                break;
+            }
+        }
+
         scan.close();
     }
 }
